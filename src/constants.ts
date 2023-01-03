716a6a38025e5f4f816f9ae008a172cbbbcbf2c2
James Stuart Milne <jstuartmilne@gmail.com>
2023-01-03 15:24:31 -0300

    refactor: updated constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const SUFFIX = ""
+const SUFFIX = `${process.env.NSUFFIX}`;
 export const GET_USER_INFO =  `get_user_info${SUFFIX}`;
 export const SEND_EMAIL = `send_email${SUFFIX}`;
 export const SEND_SMS =  `send_sms${SUFFIX}`;
